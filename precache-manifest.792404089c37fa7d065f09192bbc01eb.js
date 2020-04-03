fbf7042bdb8977374f031aa398f403919ccf271e
RoLedXD <44301044+roledxd@users.noreply.github.com>
2020-04-03 16:03:04 +0300

    Add files via upload

---
+++ precache-manifest.792404089c37fa7d065f09192bbc01eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302843c9830d7de30c41cd5174dd29b3",
+    "url": "/kusticoins-app/index.html"
+  },
+  {
+    "revision": "4ea5bb7c39d6d02372c1",
+    "url": "/kusticoins-app/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "7c90159269cfdd2947ef",
+    "url": "/kusticoins-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4ea5bb7c39d6d02372c1",
+    "url": "/kusticoins-app/static/js/2.e4b0e6e8.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/kusticoins-app/static/js/2.e4b0e6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c90159269cfdd2947ef",
+    "url": "/kusticoins-app/static/js/main.2b7b8f82.chunk.js"
+  },
+  {
+    "revision": "8c97473c45f9fa34722f",
+    "url": "/kusticoins-app/static/js/runtime-main.f9a91c50.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/kusticoins-app/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fc45eca07ba3ebd371c035417012d8e5.js"
+  "/kusticoins-app/precache-manifest.792404089c37fa7d065f09192bbc01eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kusticoins-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
