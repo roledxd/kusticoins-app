2d4f9f89387634e7097f1e59273f44f6cb99197f
RoLedXD <44301044+roledxd@users.noreply.github.com>
2020-04-03 15:51:20 +0300

    Add files via upload

---
+++ precache-manifest.fc45eca07ba3ebd371c035417012d8e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c477343435c9ba3fa0ae6e760a05cf71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d41d587411a6a6409f76",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "7c90159269cfdd2947ef",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d41d587411a6a6409f76",
+    "url": "./static/js/2.ae1f3142.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.ae1f3142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c90159269cfdd2947ef",
+    "url": "./static/js/main.2b7b8f82.chunk.js"
+  },
+  {
+    "revision": "db44f6b52bfba7b59a38",
+    "url": "./static/js/runtime-main.d854bc5c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.daafb8bf6cba9d414d773f7217b99c39.js"
+  "./precache-manifest.fc45eca07ba3ebd371c035417012d8e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
